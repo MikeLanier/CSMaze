54e41ae642cd55904d109b872fc2bbf13e328e30
Mike Lanier <Mike.Lanier1@comcast.net>
2017-09-30 17:30:11 -0500

    Creating controls in code rather than in XAML

--- CSMaze/MainWindow.xaml.cs
+++ CSMaze/MainWindow.xaml.cs
@@ -23,6 +23,65 @@ namespace CSMaze
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			StackPanel spVertical = new StackPanel();
+			if (spVertical != null)
+			{
+				spVertical.HorizontalAlignment = HorizontalAlignment.Left;
+				spVertical.VerticalAlignment = VerticalAlignment.Top;
+
+				mainGrid.Children.Add(spVertical);
+
+				StackPanel spLoadControl = new StackPanel();
+				if (spLoadControl != null)
+				{
+					spLoadControl.Orientation = Orientation.Horizontal;
+					spVertical.Children.Add(spLoadControl);
+
+					MazeControlPanelButton btnLoad = new MazeControlPanelButton("Load", 60, 20);
+					btnLoad.Click += new RoutedEventHandler(onLoadButtonClick);
+					spLoadControl.Children.Add(btnLoad);
+
+					MazeControlPanelTextBox tbLoadFilename = new MazeControlPanelTextBox(150, 20);
+					spLoadControl.Children.Add(tbLoadFilename);
+
+					MazeControlPanelButton btnFileLoadDialog = new MazeControlPanelButton("?", 20, 20);
+					spLoadControl.Children.Add(btnFileLoadDialog);
+				}
+
+				StackPanel spSaveControl = new StackPanel();
+				if (spSaveControl != null)
+				{
+					spSaveControl.Orientation = Orientation.Horizontal;
+					spVertical.Children.Add(spSaveControl);
+
+					MazeControlPanelButton btnSave = new MazeControlPanelButton("Save", 60, 20);
+					spSaveControl.Children.Add(btnSave);
+
+					MazeControlPanelTextBox tbSaveFilename = new MazeControlPanelTextBox(150, 20);
+					spSaveControl.Children.Add(tbSaveFilename);
+
+					MazeControlPanelButton btnFileSaveDialog = new MazeControlPanelButton("?", 20, 20);
+					spSaveControl.Children.Add(btnFileSaveDialog);
+				}
+
+				StackPanel spMazesControl = new StackPanel();
+				if (spMazesControl != null)
+				{
+					spMazesControl.Orientation = Orientation.Horizontal;
+					spVertical.Children.Add(spMazesControl);
+
+					spMazesControl.Children.Add(new MazeControlPanelTextBlock("Mazes", 60, 20));
+
+					MazeControlPanelTextBox tbSaveFilename = new MazeControlPanelTextBox(150, 20);
+					spMazesControl.Children.Add(tbSaveFilename);
+				}
+			}
+		}
+
+		void onLoadButtonClick(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
---
+++ CSMaze/MazeControlPanelTextBlock.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CSMaze
+{
+	public class MazeControlPanelTextBlock: System.Windows.Controls.TextBlock
+	{
+		public MazeControlPanelTextBlock(string title, double width, double height)
+		{
+			Text = title;
+			Width = width;
+			Height = height;
+			Margin = new System.Windows.Thickness(5, 5, 5, 5);
+		}
+	}
+}
