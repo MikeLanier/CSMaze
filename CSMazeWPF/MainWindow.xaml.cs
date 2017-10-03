b10d4880169aad9288fd8cea4e19c9cc3ae6592d
Mike Lanier <Mike.Lanier1@comcast.net>
2017-10-03 10:53:39 -0500

    Add the WPF version as a separate executable

---
+++ CSMazeWPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace CSMaze
+{
+	public partial class MainWindow : Window
+	{
+		Polygon myPolygon = null;
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			int size = 50;
+			int xOffset = 50;
+			int yOffset = 50;
+
+			MazeWall wallh;
+			MazeWall wallv;
+
+			for (int x = 0; x <= 10; x++)
+			{
+				for (int y = 0; y <= 10; y++)
+				{
+					if (x < 10)
+					{
+						wallh = new CSMaze.MazeWall(x * size + xOffset, y * size + yOffset, size, true);
+						displayPanel.Children.Add(wallh.graphic());
+					}
+					if (y < 10)
+					{
+						wallv = new CSMaze.MazeWall(x * size + xOffset, y * size + yOffset, size, false);
+						displayPanel.Children.Add(wallv.graphic());
+					}
+				}
+			}
+		}
+	}
+}
---
+++ CSMazeWPF/MazeWall.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CSMaze
+{
+	public class MazeWall
+	{
+		public Polygon polygon = new Polygon();
+
+		public Polygon graphic() { return polygon;  }
+
+		public MazeWall(int x, int y, int size, bool horizonal)
+		{
+			polygon.Stroke = System.Windows.Media.Brushes.Black;
+			polygon.Fill = System.Windows.Media.Brushes.LightSeaGreen;
+			polygon.StrokeThickness = 1;
+			polygon.HorizontalAlignment = HorizontalAlignment.Left;
+			polygon.VerticalAlignment = VerticalAlignment.Top;
+
+			Point p1 = new Point();
+			Point p2 = new Point();
+			Point p3 = new Point();
+			Point p4 = new Point();
+
+			if(horizonal)
+			{
+				p1.X = x;		p1.Y = y;
+				p2.X = x+size;	p2.Y = y;
+				p3.X = x+size;	p3.Y = y + 1;
+				p4.X = x;		p4.Y = y + 1;
+			}
+			else
+			{
+				p1.X = x;		p1.Y = y;
+				p2.X = x + 1;	p2.Y = y;
+				p3.X = x + 1;	p3.Y = y + size;
+				p4.X = x;		p4.Y = y + size;
+			}
+
+			System.Console.WriteLine(p1.X.ToString() + ", " + p1.Y.ToString());
+			System.Console.WriteLine(p2.X.ToString() + ", " + p2.Y.ToString());
+			System.Console.WriteLine(p3.X.ToString() + ", " + p3.Y.ToString());
+			System.Console.WriteLine(p4.X.ToString() + ", " + p4.Y.ToString());
+
+			PointCollection myPointCollection = new PointCollection();
+			myPointCollection.Add(p1);
+			myPointCollection.Add(p2);
+			myPointCollection.Add(p3);
+			myPointCollection.Add(p4);
+			polygon.Points = myPointCollection;
+		}
+	}
+}
